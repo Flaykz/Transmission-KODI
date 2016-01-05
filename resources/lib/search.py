b5abc12f38ddc4472faa8fd56e957a2b866203b0
Flaykz <david.brouste.perso@gmail.com>
2016-01-06 10:05:39 +1100

    First commit

---
+++ resources/lib/search.py
@@ -0,0 +1,354 @@
+# -*- coding: UTF-8 -*-
+import sys
+import re
+import socket
+import requests
+import json
+from transmissionrpc.t411 import T411 as t411
+from urllib2 import urlopen, Request, URLError, HTTPError
+from urllib import quote, quote_plus, urlencode
+from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
+
+socket.setdefaulttimeout(15)
+
+class Search:
+    def __init__(self):
+        return NotImplemented
+    def search(terms):
+        return NotImplemented
+
+class Mininova(Search):
+    def __init__(self):
+        self.search_uri = 'http://www.mininova.org/rss/%s'
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote_plus(terms)
+        try :
+             f = requests.get(url)
+        except :
+            raise Exception('something wrong')
+        if (f.status_code != requests.codes.ok) :
+            f.raise_for_status()        
+        soup = BeautifulStoneSoup(f.text)
+        for item in soup.findAll('item'):
+            (seeds, leechers) = re.findall('Ratio: (\d+) seeds, (\d+) leechers', item.description.text)[0]
+            torrents.append({
+                'url': item.enclosure['url'],
+                'name': item.title.text,
+                'seeds': int(seeds),
+                'leechers': int(leechers),
+            })
+        return torrents
+
+#Bug Out of proxie ?
+class TPB(Search):
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0'
+        self.search_uri = 'https://thepiratebay.se/search/%s/'
+        self.headers = {'User-Agent': self.user_agent}
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote_plus(terms)
+        try :
+             f = requests.get(url, headers=self.headers)
+        except :
+            raise Exception('something wrong')
+        if (f.status_code != requests.codes.ok) :
+            f.raise_for_status()
+        soup = BeautifulStoneSoup(f.text)
+        for details in soup.findAll('a', {'class': 'detLink'}):
+            name = details.text
+            url = details.findNext('a', {'href': re.compile('^magnet:')})['href']
+            td = details.findNext('td')
+            seeds = int(td.text)
+            td = td.findNext('td')
+            leechers = int(td.text)
+            torrents.append({
+                'url': url,
+                'name': name,
+                'seeds': seeds,
+                'leechers': leechers,
+            })
+        return torrents
+
+class Kickass(Search):
+    def __init__(self):
+        self.search_uri = 'https://kat.cr/usearch/%s/?field=seeders&sorder=desc&rss=1'
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote_plus(terms)
+        try :
+             f = requests.get(url)
+        except :
+            raise Exception('something wrong')
+        if (f.status_code != requests.codes.ok) :
+            f.raise_for_status()
+        soup = BeautifulStoneSoup(f.text)
+        for item in soup.findAll('item'):
+            torrents.append({
+                'url': item.enclosure['url'],
+                'name': item.title.text,
+                'seeds': int(item.find('torrent:seeds').text),
+                'leechers': int(item.find('torrent:peers').text),
+            })
+        return torrents
+
+#Long, A vérifier
+class L337x(Search):
+    def __init__(self):
+        self.uri_prefix = 'http://1337x.to'
+        self.search_uri = self.uri_prefix + '/sort-search/%s/seeders/desc/1/'
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote_plus(terms)
+        try :
+             f = requests.get(url)
+        except :
+            raise Exception('something wrong')
+        if (f.status_code != requests.codes.ok) :
+            f.raise_for_status()
+        soup = BeautifulStoneSoup(f.text)
+        for details in soup.findAll('a', {'href': re.compile('^/torrent/')}):
+            div = details.findNext('div')
+            seeds = int(div.text)
+            div = div.findNext('div')
+            f_link = urlopen(self.uri_prefix + details['href'])
+            soup_link = BeautifulStoneSoup(f_link.read())
+            link = soup_link.find('a', {'href': re.compile('^magnet:')})
+            if not link:
+                continue
+            torrents.append({
+                'url': link['href'],
+                'name': details.text,
+                'seeds': seeds,
+                'leechers': int(div.text),
+            })
+        return torrents
+
+# URL ?
+class YTS(Search):
+    def __init__(self):
+        self.search_uri = 'http://yts.to/rss/%s/all/all/0'
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote(terms, '')
+        f = urlopen(url)
+        soup = BeautifulStoneSoup(f.read())
+        for item in soup.findAll('item'):
+            item_quality = item.link.text.rpartition('_')[2]
+            item_f = urlopen(item.link.text)
+            item_soup = BeautifulStoneSoup(item_f.read())
+            qualities = [s.text.strip() for s in
+                         item_soup.findAll('span', {'class': re.compile('^tech-quality')})]
+            q_index = qualities.index(item_quality)
+            span = item_soup.findAll('span', {'title': 'Peers and Seeds'})[q_index]
+            ps_pos = len(span.parent.contents) - 1
+            ps = span.parent.contents[ps_pos].split('/')
+            torrents.append({
+                'url': item.enclosure['url'],
+                'name': item.title.text,
+                'seeds': int(ps[1]),
+                'leechers': int(ps[0])
+            })
+        return torrents
+
+class Lime(Search):
+    def __init__(self):
+        self.search_uri = 'https://www.limetorrents.cc/searchrss/%s/'
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote(terms)
+        try :
+             f = requests.get(url)
+        except :
+            raise Exception('something wrong')
+        if (f.status_code != requests.codes.ok) :
+            f.raise_for_status()
+        soup = BeautifulStoneSoup(f.text)
+        for item in soup.findAll('item'):
+            (seeds, leechers) = re.findall('Seeds: (\d+) , Leechers (\d+)', item.description.text)[0]
+            torrents.append({
+                'url': item.enclosure['url'],
+                'name': item.title.text,
+                'seeds': int(seeds),
+                'leechers': int(leechers)
+            })
+        return torrents
+
+class EZTV(Search):
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0'
+        self.search_uri = 'https://eztv.ag/search/%s'
+        self.headers = {'User-Agent': self.user_agent}
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote(terms)
+        try :
+             f = requests.get(url, headers=self.headers)
+        except :
+            raise Exception('something wrong')
+        if (f.status_code != requests.codes.ok) :
+            f.raise_for_status()
+        soup = BeautifulStoneSoup(f.text)
+        for (c, item) in enumerate(soup.findAll('a', {'class': 'magnet'})):
+            if c == 30: break
+            info = item.findPrevious('a')
+            link = self.search_uri % quote(info['href'])
+            try :
+                item_f = requests.get(link, headers=self.headers)
+            except :
+                raise Exception('something wrong')
+            if (item_f.status_code != requests.codes.ok) :
+                item_f.raise_for_status()
+            item_soup = BeautifulStoneSoup(item_f.text)
+            sp = item_soup.findAll('span', {'class': re.compile('^stat_')})
+            if sp:
+                sp = [int(i.text.replace(',', '')) for i in sp]
+            else:
+                sp = [0, 0]
+            torrents.append({
+                'url': item['href'],
+                'name': info.text,
+                'seeds': sp[0],
+                'leechers': sp[1]
+            })
+        return torrents
+
+# Error 503
+class CPasBien(Search):
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0'
+        self.search_uri = 'http://www.cpasbien.io/recherche/%s.html,trie-seeds-d'
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % quote(terms)
+        req = Request(url)
+        req.add_header('User-Agent', self.user_agent)
+        f = urlopen(req)
+        if not f:
+            raise Exception('Cloudfare bloque la connexion ?')
+        else :
+            soup = BeautifulStoneSoup(f.read())
+            for item in soup.findAll('a', {'class':'titre'}):
+                name = item.text.strip()
+                url = item['href']
+                div = item.findNext('div')
+                poid = div.text
+                div = item.findNext('div').findNext('div')
+                seeds = int(div.text)
+                div = item.findNext('div').findNext('div').findNext('div')
+                leechers = int(div.text)
+                torrents.append({
+                    'url': url,
+                    'name': name,
+                    'seeds': seeds,
+                    'leechers': leechers,
+                })
+            return torrents
+##        p = []
+##        if terms.find(' ') :
+##            p = terms.split(' ')
+##        else :
+##            p.append(terms)
+##        if p[0] == "series" or p[0] == "films" :
+##            if len(p) == 1:
+##                self.search_uri = 'http://www.cpasbien.pw/view_cat.php?categorie=%s'
+##                url = self.search_uri % p[0]
+##            else :
+##                self.search_uri = 'http://www.cpasbien.pw/view_cat.php?categorie=%s&page=%s'
+##                url = self.search_uri % (p[0],p[1])
+##        else :
+##            url = self.search_uri % '-'.join(terms.split(' '))
+##		hdr = {
+##			'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4',
+##			'Accept' : 'image/webp,*/*;q=0.8',
+##			'Accept-Language' : 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
+##			'Connection' : 'keep-alive'
+##		}
+##        req = Request(url, headers=hdr)
+##        #f = urlopen(req).read()
+##        #f = urlopen(url)
+##		f = urlopen(req)
+##        soup = BeautifulSoup(f.read())
+##        for item in soup.findAll('a', {'class':'titre'}):
+##            name = item.text.strip()
+##            url = item['href']
+##            div = item.findNext('div')
+##            poid = div.text
+##            div = item.findNext('div').findNext('div')
+##            seeds = int(div.text)
+##            div = item.findNext('div').findNext('div').findNext('div')
+##            leechers = int(div.text)
+##			req = Request(url, headers=hdr)
+##            f2 = urlopen(req)
+##            soup2 = BeautifulSoup(f2.read())
+##            url = 'http://www.cpasbien.pw' + soup2.find('a', {'id':'telecharger'})['href']
+##            torrents.append({
+##                'url': url,
+##                'name': name,
+##                'seeds': seeds,
+##                'leechers': leechers,
+##            })
+##        return torrents
+        
+# Error 500
+class GetStrike(Search):
+    def __init__(self):
+        self.search_uri = 'https://getstrike.net/api/torrents/search/?q=%s'
+
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri % '+'.join(terms.split(' '))
+        hdr = {
+                    'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4',
+                    'Accept' : 'image/webp,*/*;q=0.8',
+                    'Accept-Language' : 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
+                    'Connection' : 'keep-alive'
+                    }
+        req = Request(url, headers=hdr)
+        f = urlopen(req).read()
+        rep = json.loads(f)
+        nbRes = rep[0]['results']
+        for i in rep[1] :
+            torrents.append({
+                'url': i['download_link'],
+                'name': i['torrent_title'],
+                'seeds': i['seeds'],
+                'leechers': i['leechers'],
+            })
+        return torrents
+
+class T411(Search):
+    def __init__(self):
+        self.t = t411()
+
+    def search(self, query) :
+        torrents = []
+        rep = self.t.search(query)
+        for torrent in rep['torrents'] :
+            torrents.append({"seeds":int(torrent['seeders']), "leechers":int(torrent['leechers']), "name":str(torrent['name']), "url":str(torrent['id'])})
+        return torrents
+
+if __name__ == '__main__':
+    sites = [Mininova(), Kickass(), L337x(), Lime(), EZTV(), T411()]
+    terms = 'avatar'
+    if len(sys.argv) > 1:
+        terms = sys.argv[1]
+    print 'Searching for "' + terms + '"'
+    for site in sites:
+        print site.__class__.__name__.center(79, '=')
+        torrents = site.search(terms)
+        print 'Total found = ' + str(len(torrents))
+        counter = 0
+        for torrent in torrents:
+            counter = counter + 1
+            print "seeds :" + str(torrent['seeds']) + " leechers: " + str(torrent['leechers']) + " name: " + str(torrent['name']) + " url: " + str(torrent['url'])
+            if (counter == 3): 
+                break
---
+++ resources/lib/transmissionrpc/t411.py
@@ -0,0 +1,142 @@
+# -*- coding: UTF-8 -*-
+import getpass
+import json
+import requests
+import os, xbmc, xbmcgui
+HTTP_OK = 200
+API_URL = 'https://api.t411.in/%s'
+USER_FILE = 'user.json'
+
+class T411Exception(BaseException):
+    pass
+
+class T411(object):
+    """ Base class for t411 interface """
+
+    def __init__(self, username = None, password = None) :
+        """ Get user credentials and authentificate it, if any credentials
+        defined use token stored in user file
+        """
+        #user = raw_input('Please enter username: ')
+        #password = getpass.getpass('Please enter password: ')        
+        #self._auth(user, password)
+        try :
+            with open(USER_FILE) as user_file:
+                self.user_credentials = json.loads(user_file.read())
+                if 'uid' not in self.user_credentials or 'token' not in \
+                        self.user_credentials:
+                    raise T411Exception('Wrong data found in user file')
+                #else:
+                    # we have to ask the user for its credentials and get
+                    # the token from the API
+                    #user = raw_input('Please enter username: ')
+                    #password = getpass.getpass('Please enter password: ')
+                    #self._auth(user, password)
+        except IOError as e:
+            # we have to ask the user for its credentials and get
+            # the token from the API
+            while True :
+            	user = raw_input('Please enter username: ')
+            	password = getpass.getpass('Please enter password: ')
+            	try :
+                	self._auth(user, password)
+            	except T411Exception as e:
+                	raise T411Exception(e.message)
+            	else :
+                	break
+        except T411Exception as e:
+            raise T411Exception(e.message)
+        except Exception as e:
+            raise T411Exception('Error while reading user credentials: %s.'\
+                    % e.message)
+
+    def _auth(self, username, password) :
+        """ Authentificate user and store token """
+        self.user_credentials = self.call('auth', {'username': username, 'password': password})
+        if 'error' in self.user_credentials:
+            raise T411Exception('Error while fetching authentication token: %s'\
+                    % self.user_credentials['error'])
+        # Create or update user file
+        user_data = json.dumps({'uid': '%s' % self.user_credentials['uid'], 'token': '%s' % self.user_credentials['token']})
+        with open(USER_FILE, 'w') as user_file:
+            user_file.write(user_data)
+        return True
+
+    def call(self, method = '', params = None) :
+        """ Call T411 API """
+
+        #call_params = {'url': API_URL % method, 'params': params}
+        if method == 'auth' :
+            req = requests.post(API_URL % method, data=params)
+        elif 'download' in method :
+            torrentid = os.path.basename(method)
+            req = requests.get(API_URL % method, headers={'Authorization':self.user_credentials['token']})
+            if req.status_code == requests.codes.OK :
+                torrent_data = ''
+                for block in req.iter_content(1024) :
+                    if not block :
+                        break
+                    torrent_data += block
+                return base64.b64encode(torrent_data).decode('utf-8')
+        else :
+            req = requests.post(API_URL % method, data=params, headers={'Authorization':self.user_credentials['token']})
+
+        if req.status_code == requests.codes.OK:
+            return req.json()
+        else :
+            raise T411Exception('Error while sending %s request: HTTP %s' % \
+                    (method, req.status_code))
+
+    def me(self) :
+        """ Get personal informations """
+        return self.call('users/profile/%s' % self.user_credentials['uid'])
+
+    def user(self, user_id) :
+        """ Get user informations """
+        return self.call('users/profile/%s' % user_id)
+
+    def categories(self) :
+        """ Get categories """
+        return self.call('categories/tree')
+
+    def terms(self) :
+        """ Get terms """
+        return self.call('terms/tree')
+
+    def details(self, torrent_id) :
+        """ Get torrent details """
+        return self.call('torrents/details/%s' % torrent_id)
+
+    def download(self, torrent_id) :
+        """ Download a torrent """
+        return self.call('torrents/download/%s' % torrent_id)
+
+    def search(self, query) :
+        """ Search a torrent """
+        return self.call('torrents/search/%s' % query)
+
+    def top100(self) :
+        return self.call('torrents/top/100')
+
+    def topToday(self) :
+        return self.call('torrents/top/today')
+
+    def topWeek(self) :
+        return self.call('torrents/top/week')
+
+    def topMonth(self) :
+        return self.call('torrents/top/month')
+
+    def get_bookmarks(self) :
+        return self.call('bookmarks')
+
+    def add_bookmark(self, torrent_id) :
+        """ Get bookmarks of user """
+        return self.call('bookmarks/save/%s' % torrent_id)
+         
+    def delete_bookmark(self, torrent_id) :
+        """ Delete a bookmark """
+        return self.call('bookmarks/delete/%s' % torrent_id)
+if __name__ == "__main__" :
+    t411 = T411() 
+    print t411.top100()
